fb3c5d1d3090086fcb5341f715966a2db4044a39
Artur Baruchi <artur.baruchi@gmail.com>
2016-09-01 15:59:59 -0300

    Update: New enum ofp_table_mod_failed_code - Issue #146

--- pyof/v0x02/asynchronous/error_msg.py
+++ pyof/v0x02/asynchronous/error_msg.py
@@ -1,3 +1,6 @@
+# System imports
+from enum import Enum
+
 """Defines an Error Message."""
 from pyof.v0x01.asynchronous.error_msg import (BadActionCode, BadRequestCode,
                                                ErrorMsg, ErrorType,
@@ -5,3 +8,38 @@ from pyof.v0x01.asynchronous.error_msg import (BadActionCode, BadRequestCode,
                                                HelloFailedCode,
                                                PortModFailedCode,
                                                QueueOpFailedCode)
+
+class ErrorType(Enum):
+    """Values for ’type’ in ofp_error_message.
+
+    These values are immutable: they will not change in future versions of the
+    protocol (although new values may be added).
+    """
+
+    #: Hello protocol failed
+    OFPET_HELLO_FAILED = 0,
+    #: Request was not understood
+    OFPET_BAD_REQUEST = 1,
+    #: Error in action description
+    OFPET_BAD_ACTION = 2,
+    #: Problem in modifying Flow entry
+    OFPET_FLOW_MOD_FAILED = 3,
+    #: Problem in modifying Port entry
+    OFPET_PORT_MOD_FAILED = 4,
+    #: New - added in v0x02
+    #: Problem in handling tables
+    OFPET_TABLE_MOD_FAILED = 5,
+    #: Problem in modifying Queue entry
+    OFPET_QUEUE_OP_FAILED = 6
+
+
+class TableModFailed(Enum):
+    """Error msg 'code' values for OFPET_QUEUE_OP_FAILED.
+
+    'data' contains at least the first 64 bytes of the failed request.
+    """
+
+    #: Specified table does not exist
+    OFPTMFC_BAD_TABLE = 0,
+    #: Specified config does not exist
+    OFPTMFC_BAD_CONFIG = 1
