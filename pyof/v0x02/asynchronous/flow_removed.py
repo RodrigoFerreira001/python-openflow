6d6ad9d305e6039a78b514083a22b90239792d1c
Artur Baruchi <artur.baruchi@gmail.com>
2016-09-05 15:57:11 -0300

    enum ofp_flow_removed_reason: new value (last one) - Issue #146

--- pyof/v0x02/asynchronous/flow_removed.py
+++ pyof/v0x02/asynchronous/flow_removed.py
@@ -1,2 +1,22 @@
-"""The controller has requested to be notified when flows time out."""
-from pyof.v0x01.asynchronous.flow_removed import FlowRemoved, FlowRemovedReason
+# System imports
+from enum import Enum
+
+from pyof.v0x01.asynchronous.flow_removed import FlowRemoved
+
+__all__ = ('FlowRemoved', 'FlowRemovedReason')
+
+# Enums
+
+
+class FlowRemovedReason(Enum):
+    """Why the flow was removed."""
+
+    #: Flow idle time exceeded idle_timeout
+    OFPRR_IDLE_TIMEOUT = 0
+    #: Time exceeded hard_timeout
+    OFPRR_HARD_TIMEOUT = 1
+    #: Evicted by a DELETE flow mod
+    OFPRR_DELETE = 2
+    #: New - added in v0x02
+    #: Group was removed
+    OFPRR_GROUP_DELETE = 3
