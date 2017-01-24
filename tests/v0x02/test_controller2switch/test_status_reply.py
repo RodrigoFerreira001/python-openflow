a7f1294b2999d9f17ca7ace1206a4d74f4a0144f
Macartur Sousa <macartur.sc@gmail.com>
2017-01-24 14:12:03 -0200

    Adding dummy tests to verify inherance
    - Adding tests to v0x02/tests_controller2switch/test_hello.py
    - Adding tests to v0x02/tests_controller2switch/test_status_reply.py

---
+++ tests/v0x02/test_controller2switch/test_status_reply.py
@@ -0,0 +1,20 @@
+"""Testing StatusReply class."""
+from pyof.v0x02.controller2switch.common import StatsTypes
+from pyof.v0x02.controller2switch.stats_reply import StatsReply
+
+from tests.test_struct import TestStruct
+
+
+class TestStatsReply(TestStruct):
+    """Test for StatsReply message."""
+
+    @classmethod
+    def setUpClass(cls):
+        """[Controller2Switch/StatsReply] - size 12."""
+        super().setUpClass()
+        super().set_raw_dump_file('v0x02', 'ofpt_stats_reply')
+        super().set_raw_dump_object(StatsReply, xid=1,
+                                    body_type=StatsTypes.OFPST_GROUP,
+                                    flags=0x0001, body=[])
+        super().set_minimum_size(16)
+        print('*'*2000)
